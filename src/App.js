210f2cefdb84b88c2b3fd54e9f4515f5baca9fd7
unknown <honorekarikumutima.com>
2024-12-23 11:52:23 +0200

    Rendering Categories and import the styling of e-store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React, {useState} from 'react';
+import './App.css';
+
+import Category from './components/Category';
+
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [results, setResults] = useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3001/categories')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setResults(data);
+      })
+  }, []);
+
+  const renderCategories = () => {
+    const categories = [];
+    for (let i = 0; i < results.length; i++) {
+      categories.push(<Category key={results[i].id} id={results[i].id} title={results[i].title} />);
+    }
+
+    return categories;
+  }
   return (
-    <div>
-      <h1>Welcome to our store!</h1>
-      <p>Feel free to browse our products.</p>
-    </div>
+    <>
+    <header>My Store</header>
+    <section>
+      <nav>
+      {
+        results && renderCategories()
+      }
+      </nav>
+      <article>
+        main content
+      </article>
+    </section>
+    <footer>
+      footer
+    </footer>
+    </>
   );
 }
 
